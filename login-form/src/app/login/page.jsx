b952280591fca7cb0d6b230124862564edcef47f
Kalpana_Perera <118042669+KalpanaPerera@users.noreply.github.com>
2025-06-19 18:57:32 +0530

    login

---
+++ login-form/src/app/login/page.jsx
@@ -0,0 +1,9 @@
+import LoginForm from "@/components/LoginForm";
+
+export default function LoginPage() {
+  return (
+    <main className="flex items-center justify-center min-h-screen bg-gray-100 dark:bg-neutral-950 p-4">
+      <LoginForm />
+    </main>
+  );
+}
